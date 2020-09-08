1d148ab2f556530a78dd8e7fd5fb56c058b0fa2a
EdJoPaTo <git-commit-with-a-funny-long-email-address-everyone-hates-as-it-is-too-long@edjopato.de>
2020-09-08 07:51:04 +0200

    refactor: cleanup code

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
-use mpd::{Client, State};
+use mpd::{Client, State, Status};
 use std::thread::sleep;
 use std::time::Duration;
 
-const BETWEEN_CHECKS: Duration = Duration::from_secs(15);
+const BETWEEN_CHECKS: Duration = Duration::from_secs(30);
 const BETWEEN_ELAPSED: Duration = Duration::from_millis(400);
 
 fn main() {
     let mut conn = Client::connect("127.0.0.1:6600").expect("failed to connect to mpd");
+
+    println!("Connected. Running...");
 
     loop {
         check(&mut conn).expect("failed to perform check");
@@ -15,19 +17,33 @@ fn main() {
 }
 
 fn check(conn: &mut Client) -> Result<(), mpd::error::Error> {
-    let status = conn.status()?;
-    println!("check {:?} {:?}", status.state, status.duration);
-    if status.state == State::Play && status.duration == None {
-        let before = status.elapsed;
-        sleep(BETWEEN_ELAPSED);
-        let after = conn.status()?.elapsed;
-        println!("before & after {:?} {:?}", before, after);
+    let before = conn.status()?;
 
-        if before == after {
+    #[cfg(debug_assertions)]
+    println!(
+        "before  {:?} {:?} {:?}",
+        before.state, before.elapsed, before.duration
+    );
+
+    if is_internet_radio_playing(&before) {
+        sleep(BETWEEN_ELAPSED);
+        let after = conn.status()?;
+
+        #[cfg(debug_assertions)]
+        println!(
+            "after   {:?} {:?} {:?}",
+            after.state, after.elapsed, after.duration
+        );
+
+        if is_internet_radio_playing(&after) && before.elapsed == after.elapsed {
             conn.stop()?;
             conn.play()?;
         }
     }
 
     Ok(())
+}
+
+fn is_internet_radio_playing(status: &Status) -> bool {
+    status.state == State::Play && status.duration == None
 }
