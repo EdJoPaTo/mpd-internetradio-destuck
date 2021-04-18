aa1c6f383ecfa5b93d13d1dc9fb6649dcb8ea449
EdJoPaTo <git-commit-with-a-funny-long-email-address-everyone-hates-as-it-is-too-long@edjopato.de>
2021-04-18 15:24:23 +0200

    chore(install): improve install script
    - install service also to /usr/local
    - enable and (re)start after build

--- install.sh
+++ install.sh
@@ -1,10 +1,14 @@
-#!/bin/sh
+#!/usr/bin/env bash
+set -e
 
 nice cargo build --release
-sudo cp -uv target/release/mpd-internetradio-destuck /usr/local/bin/
 
-# copy stuff
-sudo cp -uv *.service /etc/systemd/system
-
-# reload systemd
+# systemd
+sudo mkdir -p /usr/local/lib/systemd/system/
+sudo cp -uv ./*.service /usr/local/lib/systemd/system/
 sudo systemctl daemon-reload
+
+# stop, replace and start new version
+sudo systemctl stop mpd-internetradio-destuck.service
+sudo cp -v target/release/mpd-internetradio-destuck /usr/local/bin
+sudo systemctl enable --now mpd-internetradio-destuck.service
